0b192c123def2b02fec4ea1118511b5a7f20ebe4
josh moreno <morenojosh1823@gmail.com>
2026-03-24 13:32:01 -0700

    cleanup

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,9 +1,3 @@
-// // pages/logout.tsx
-// import { apiFetch } from '@/utils/api';
-// import { setCookie } from 'cookies-next';
-// import { GetServerSideProps } from 'next';
-
-// pages/logout.tsx
 import { apiFetch } from '@/utils/api';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
@@ -30,53 +24,8 @@ export default function Logout({ apiUrl }: { apiUrl: string }) {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    const apiUrl = process.env.SHTAI_API_URL!;
     console.log('Logging out user...');
     return {
         props: { apiUrl: '/api/backend' }, // Pass the API URL
     };
 };
-
-// export const getServerSideProps: GetServerSideProps = async ({ res, req }) => {
-//   try {
-//     console.log('Logging out user...');
-
-//     await apiFetch(`${process.env.SHTAI_API_URL!}/auth/logout`, {
-//       method: 'POST',
-//       headers: {
-//         cookie: req.headers.cookie || '', // forward incoming cookies
-//       },
-//     });
-
-//     const isDeployed = process.env.ENV === 'prod' || process.env.ENV === 'stage';
-
-//     setCookie('SHTAIToken', '', {
-//       req,
-//       res,
-//       maxAge: -1, // Set cookie to expire immediately
-//       path: '/',
-//       secure: isDeployed, // Use secure cookies in production
-//       sameSite: isDeployed ? 'none' : 'lax', // Use lax SameSite policy
-//       httpOnly: true, // Make cookie HTTP-only for security
-//     });
-
-//     return {
-//       redirect: {
-//         destination: '/',
-//         permanent: false,
-//       },
-//     };
-//   } catch (error) {
-//     console.error('Logout failed:', error);
-//     return {
-//       redirect: {
-//         destination: '/',
-//         permanent: false,
-//       },
-//     }
-//   };
-// };
-
-// export default function Logout() {
-//   return null; // This page will never actually render
-// }
